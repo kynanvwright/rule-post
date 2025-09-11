593837b3557344c3d2761a4a5be0d8902e2b05b0
Kynan Wright <kynan.wright@emiratesteamnz.com>
2025-09-12 09:23:27 +1200

    update: limit simultaneous cloud function instances

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,14 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// import {setGlobalOptions} from "firebase-functions";
-// import {onRequest} from "firebase-functions/https";
-// import * as logger from "firebase-functions/logger";
+import {setGlobalOptions} from "firebase-functions";
 import {beforeUserCreated} from "firebase-functions/v2/identity";
 import {HttpsError} from "firebase-functions/v2/https";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
@@ -22,17 +18,7 @@ import {HttpsError} from "firebase-functions/v2/https";
 // per-function limit. You can override the limit for each function using the
 // `maxInstances` option in the function's options, e.g.
 // `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-// setGlobalOptions({maxInstances: 10});
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
+setGlobalOptions({maxInstances: 10});
 
 // Hard-block ALL end-user registrations (email/password, OAuth, REST signUp)
 export const blockAllSelfRegistration = beforeUserCreated((_event) => {
