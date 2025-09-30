e22eeb0798060b27d45f364d488a652efbe32a7e
Kynan Wright <kynan.wright@emiratesteamnz.com>
2025-09-30 15:31:44 +1300

    fix: update deprecated code

--- flutter_app/lib/core/widgets/colour_helper.dart
+++ flutter_app/lib/core/widgets/colour_helper.dart
@@ -5,9 +5,9 @@ extension ColorUtils on Color {
   Color lighten([double amount = 0.1]) {
     assert(amount >= 0 && amount <= 1);
     return withValues(
-      red:   red / 255 + (1 - red / 255) * amount,
-      green: green / 255 + (1 - green / 255) * amount,
-      blue:  blue / 255 + (1 - blue / 255) * amount,
+      red:   r + (1 - r) * amount,
+      green: g + (1 - g) * amount,
+      blue:  b + (1 - b) * amount,
     );
   }
 }
