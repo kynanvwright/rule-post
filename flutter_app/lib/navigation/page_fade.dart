03dc0265ac4b1f8c4761d3d5ebb459a78884c084
Kynan Wright <kynan.wright@emiratesteamnz.com>
2025-10-16 16:09:31 +1300

    feat: option to fade pages in

---
+++ flutter_app/lib/navigation/page_fade.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+// use in main.dart if you want pages to fade in on transition
+CustomTransitionPage fadePage(GoRouterState state, Widget child) {
+  return CustomTransitionPage(
+    key: state.pageKey,
+    transitionDuration: const Duration(milliseconds: 500),
+    reverseTransitionDuration: const Duration(milliseconds: 500),
+    transitionsBuilder: (ctx, anim, secAnim, child) =>
+        FadeTransition(opacity: anim, child: child),
+    child: child,
+  );
+}
