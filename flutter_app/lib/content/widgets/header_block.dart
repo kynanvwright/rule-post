1f77c6f8616743730f3b05d0fe75251968569ece
Marc <marc.wintermantel@gmail.com>
2026-03-17 11:58:13 +0000

    Make detail header text selectable

--- flutter_app/lib/content/widgets/header_block.dart
+++ flutter_app/lib/content/widgets/header_block.dart
@@ -1,7 +1,6 @@
 // flutter_app/lib/content/widgets/header_block.dart
 import 'package:flutter/material.dart';
 
-
 // header content for the enquiry and response detail pages
 class HeaderBlock extends StatelessWidget {
   const HeaderBlock({
@@ -23,25 +22,26 @@ class HeaderBlock extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         for (final sub in subHeaderLines)
-          Text(
+          SelectableText(
             '$sub:',
             style: t.titleMedium?.copyWith(fontStyle: FontStyle.italic),
           ),
         if (subHeaderLines.isNotEmpty) const SizedBox(height: 4),
-        for (final line in headerLines) Text(line, style: t.titleLarge),
+        for (final line in headerLines)
+          SelectableText(line, style: t.titleLarge),
       ],
     );
 
     // 👇 If trailing is provided, wrap in Row; otherwise just show header text.
-    return trailing == null ?
-    headerColumn :
-    Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(child: headerColumn),
-        const SizedBox(width: 8),
-        trailing!,
-      ],
-    );
+    return trailing == null
+        ? headerColumn
+        : Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(child: headerColumn),
+              const SizedBox(width: 8),
+              trailing!,
+            ],
+          );
   }
 }
